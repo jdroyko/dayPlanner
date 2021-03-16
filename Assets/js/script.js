2dd2b73d302f051965d7957fe65cc8c065737a5d
unknown <c15jeanne.domingo@gmail.com>
2021-03-16 09:46:21 -0400

    Added date stamp on the header.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,16 +1,17 @@
 $(document).ready(function () {
 
-    //Add button click
-    $('.saveBtn').on('click', function () {
-        var value = $(this).siblings('.description').val()
-        var time$(this).parent.attr('id');
+    // //Add button click
+    // $('.saveBtn').on('click', function () {
+    //     var value = $(this).siblings('.description').val()
+    //     var time$(this).parent.attr('id');
 
-        //Set item to local storage
-    })
+    //     //Set item to local storage
+    // })
 
     //function for bringing in moment and appennding to page
 
-
+    var today = moment();
+    $("#1a").text(today.format("ddd MMM Do, YYYY"));
 
 
 
